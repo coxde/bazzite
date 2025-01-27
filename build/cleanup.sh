57ee817ab4d6d55a15d2f18b7a394c44a3af0b18
COxDE <63153334+coxde@users.noreply.github.com>
2025-01-27 18:46:57 +0000

    feat(script): more clean up (#6)
    * feat(script): more clean up
    
    * fix: enable extglob

--- build/cleanup.sh
+++ build/cleanup.sh
@@ -17,3 +17,12 @@ EOF
 # Replace ublue-update with uupd
 systemctl disable ublue-update.timer
 systemctl enable uupd.timer
+
+# Clean temp files
+# Enable extended globbing
+shopt -s extglob
+
+dnf5 clean all
+rm -rf /tmp/* || true
+rm -rf /var/!(cache)
+rm -rf /var/cache/!(rpm-ostree)
